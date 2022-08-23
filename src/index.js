8d05936b4c9c08572ec99dfc5c3c22af18700fcb
Gabriel Batista <gabriel_batista@trimble.com>
2022-08-23 14:22:36 -0300

    project foudations, and understanding express usage

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get("/", (request, response) => {
+    // return response.send("Hello World!");
+    return response.json({message: "Hello World!"});
+})
+
+app.listen(8232); // this means => localhost:8232
